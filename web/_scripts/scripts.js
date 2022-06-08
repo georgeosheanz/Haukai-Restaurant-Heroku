76b2d020d22080d92be486d3ceb88d5d5a9fa188
georgeosheanz <george.oshea@icloud.com>
2022-06-08 16:13:54 +1200

    Private Page Format

--- web/_scripts/scripts.js
+++ web/_scripts/scripts.js
@@ -16,3 +16,12 @@ function myFunction2() {
     x.style.display = "none";
   }
 }
+
+function myFunction(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
